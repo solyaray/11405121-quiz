2112256f621d239b25c89299802aab975f38b716
solyaray <122314201+solyaray@users.noreply.github.com>
2023-12-14 14:18:52 +0300

    Add files via upload

---
+++ Task01/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[22];
+int sum = 0;
+int ans;
+
+void backtrack(int i, int s) {
+  if (i == n) {
+    ans = min(ans, abs(sum - s * 2));
+    return;
+  }
+  backtrack(i + 1, s);
+  backtrack(i + 1, s + arr[i]);
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &arr[i]);
+    sum += arr[i];
+  }
+  ans = sum;
+  backtrack(0, 0);
+  printf("%d\n", ans);
+  return 0;
+}
